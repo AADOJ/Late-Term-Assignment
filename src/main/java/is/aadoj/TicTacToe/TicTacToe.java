325d2a35687aca4c0bfe9a453fa5177706bd7800
Gudjon Geir Jonsson <gudjongeir@gmail.com>
2014-10-28 21:42:56 +0000

    Added api for TicTacToe.java

---
+++ src/main/java/is/aadoj/TicTacToe/TicTacToe.java
@@ -0,0 +1,33 @@
+package is.aadoj.TicTacToe;
+
+public class TicTacToe
+{
+	private int round;
+	private char[] board;
+	boolean p1Won;
+	boolean p2Won;
+
+	public TicTacToe()
+	{
+		round = 1;
+		board = new char[9];
+		p1Won = false;
+		p2Won = false;
+	}
+
+	// inserts a symbol at target location
+	public char[] insertChar(int location)
+	{
+		return board;
+	}
+
+	public bool gameFinished()
+	{
+		return false;
+	}
+
+	public int whoWon()
+	{
+		return 0;
+	}
+}
