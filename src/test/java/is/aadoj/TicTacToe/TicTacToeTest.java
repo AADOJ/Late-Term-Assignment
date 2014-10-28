2958b04e2d67185d453d212d444f4680cfa0ef52
Gudjon Geir Jonsson <gudjongeir@gmail.com>
2014-10-28 21:50:11 +0000

    Created TicTacToeTest.java file

---
+++ src/test/java/is/aadoj/TicTacToe/TicTacToeTest.java
@@ -0,0 +1,15 @@
+package is.aadoj.TicTacToe;
+
+import static org.junit.Assert.*;
+
+public class TicTacToeTest
+{
+	public stati void main(String args[])
+	{
+		@Test
+		public void exampleTest()
+		{
+			assertEquals(0, 0);
+		}
+	}
+}
