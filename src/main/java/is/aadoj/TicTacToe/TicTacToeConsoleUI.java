0a49d834ab6ba6eca8b90fd61eabcaee1cf08677
Gudjon Geir Jonsson <gudjongeir@gmail.com>
2014-10-30 19:03:25 +0000

    Created a rudimentary console UI

--- src/main/java/is/aadoj/TicTacToe/TicTacToeConsoleUI.java
+++ src/main/java/is/aadoj/TicTacToe/TicTacToeConsoleUI.java
@@ -6,8 +6,31 @@ public class TicTacToeConsoleUI
 {
 	public static void main(String args[])
 	{
-		TicTacToe game = new TicTacToe();
-		System.out.println(game.printBoard());
+		Scanner in = new Scanner(System.in);
+		boolean gameOn = true;
+		while(gameOn)
+		{
+			TicTacToe game = new TicTacToe();
+			System.out.println("Welcome to TicTacToe, insert a number from 0-8 to place your mark.");
+			while(!game.gameFinished())
+			{
+				System.out.println(game.printBoard());
+				game.insertChar(in.nextInt());
+			}
+			if (game.whoWon(true, false) == 0) 																// TODO:Laga whoWon
+				System.out.println("It's a tie!");
+			else
+				System.out.println("Player " + game.whoWon(true, false) + " won!");							// TODO:Laga whoWon
+
+			System.out.println("Another game? (Y/N)");
+			String newGame = in.next();
+			while(!(newGame.equals("Y") || newGame.equals("y") || newGame.equals("N") || newGame.equals("n")))
+				System.out.println("Incorrect input, use either Y or N");
+
+			if (newGame.equals("N") || newGame.equals("n"))
+				gameOn = false;
+		}
+		
 	}
 
 	
