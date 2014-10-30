fc672fb58b3142069377b3a46cbc898173d4175e
Gudjon Geir Jonsson <gudjongeir@gmail.com>
2014-10-30 22:23:33 +0000

    Fixed issue with round when inserting on an occupied slot Pt. 2

--- src/main/java/is/aadoj/TicTacToe/TicTacToe.java
+++ src/main/java/is/aadoj/TicTacToe/TicTacToe.java
@@ -33,8 +33,8 @@ public class TicTacToe
 
 		if(board[location] == 'X' || board[location] == 'O') 
 		{
-			throw new SlotOccupiedException();
 			round--;
+			throw new SlotOccupiedException();	
 		}
 		board[location] = symbol;
 
