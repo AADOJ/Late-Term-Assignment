58f1e100077ce503495983a57048ffc8cf67c677
omaroskars <omar13@ru.is>
2014-11-02 17:10:24 +0000

    Added UI navigation button and new game function for h vs c

--- src/main/resources/public/js/scriptC.js
+++ src/main/resources/public/js/scriptC.js
@@ -2,8 +2,8 @@ var turn;
 var gameArray;
 var round;
 var boardLocked;
-var xWins;
-var yWins;
+var xWins = 0;
+var oWins = 0;
 
 
 $(document).ready(function(){
@@ -18,12 +18,18 @@ $(document).ready(function(){
 		} else if(!boardLocked && ifOccupied(tileNum)) {
 			messageToUser("Tile is occupied!");
 		}
-
-
 	});
 
 	$("#restartButt").click(function(){
 		restart();
+	});
+
+	$("#newGame").click(function() {
+		newGame();
+	});
+
+	$("#chooseGame").click(function() {
+		window.location.href = "index.html";
 	});
 
 });
@@ -81,15 +87,19 @@ function decideUpponResponse(response){
 	if(response.gameFinished){
 		boardLocked = true;
 		if(response.winner == 1){
+			xWins++;
 			messageToUser("You win!");
+			$("#xwins").text(xWins0);
 		} else if(response.winner == 2) {
+			oWins++;
 			messageToUser("Computer win!");
+			$("#owins").text(oWins);
 		} else {
 			messageToUser("It's a draw!");
 		}
 
 	}else{
-		var tileNum = response.computerMove();
+		var tileNum = response.computerMove;
 		drawOnBoard(tileNum);
 	}
 }
@@ -109,4 +119,12 @@ function ajaxCall(){
             decideUpponResponse(jQuery.parseJSON(data));
             boardLocked = false;
         }); 
+}
+
+function newGame() {
+	xWins = 0;
+	oWins = 0;
+	restart();
+	$("#xwins").html(xWins);
+	$("#owins").html(oWins);
 }
